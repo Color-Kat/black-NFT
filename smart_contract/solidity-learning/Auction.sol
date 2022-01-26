0a26f2fb890dbdefb0ab07ebf3153253dd96b657
Color-Kat <katik7979@gmail.com>
2022-01-26 21:29:01 +0300

    i do not understand how should it work

--- smart_contract/solidity-learning/Auction.sol
+++ smart_contract/solidity-learning/Auction.sol
@@ -20,6 +20,7 @@ contract AuctionBox{
 
     // call event when auction is created
     event AuctionCreated(Auction _auction);
+    event Log(address _message);
 
     // add to auctions array new Auction instance
     function createAuction(
@@ -34,6 +35,8 @@ contract AuctionBox{
 
         emit AuctionCreated(newAuction);
 
+        emit Log(newAuction.ownerOf(newAuction.tokenCounter() - 1 ));
+
         auctions.push(newAuction); // add auction to list
     }
 
@@ -46,8 +49,8 @@ contract AuctionBox{
         return auctions[_id];
     }
 
-    function getAuctionNFTById(uint256 _id) public view returns (Auction) {
-        return auctions[_id].tokenURI;
+    function getAuctionTokenURIById(uint256 _id) public view returns (string memory) {
+        return auctions[_id].tokenURI(_id);
     }
 
     // return content of auction by id
@@ -56,7 +59,31 @@ contract AuctionBox{
     }
 }
 
-contract Auction {
+contract NFT is ERC721URIStorage{
+    uint256 public tokenCounter; // id of current 
+
+    constructor() ERC721("NiggaNFT", "Nigga") {
+        tokenCounter = 0; // count of NFT equals 0 when we deploy contract
+    }
+
+    //  constructor(string memory name, string memory symbol) ERC721(name, symbol) {
+    //     tokenCounter = 0; // count of NFT equals 0 when we deploy contract
+    // }
+
+    function mintNFT(string memory tokenURI) public returns (uint256){
+        uint256 newItemId = tokenCounter;
+
+        // create new NFT by owner(sender) and new NFT id
+        _mint(msg.sender, newItemId);
+        _setTokenURI(newItemId, tokenURI);
+        
+        tokenCounter++; // increase count of NFT
+
+        return newItemId;
+    }
+}
+
+contract Auction is NFT {
     address payable private owner;
     string public title;
     uint256 public startPrice;
@@ -77,9 +104,6 @@ contract Auction {
     // bids list
     mapping(address => uint256) public bids;
 
-    // id of generated nft token;
-    uint256 public nftTokenId;
-
     constructor(
         address payable _owner,
         string memory _title,
@@ -96,8 +120,11 @@ contract Auction {
         endTime = block.timestamp + _duration; // count end date (now + auction duration)
 
         // create new NFT product
-        NFT nft = new NFT();
-        nftTokenId = nft.mintNFT(_tokenURI);
+        // NFT nft = new NFT();
+        // nftTokenId = nft.mintNFT(_tokenURI);
+
+        // tokenCounter = 0;
+        mintNFT(_tokenURI);
     }
 
     // just return title of this auction
@@ -111,7 +138,7 @@ contract Auction {
             owner,
             title,
             descriprion,
-            nftTokenId,
+            tokenCounter,
             startPrice,
             endTime
         );
@@ -133,31 +160,6 @@ contract Auction {
     //         endTime
     //     );
     // }
-}
-
-contract NFT is ERC721URIStorage{
-    uint256 public tokenCounter; // id of current 
-
-    constructor() ERC721("NiggaNFT", "Nigga") {
-        tokenCounter = 0; // count of NFT equals 0 when we deploy contract
-    }
-
-    //  constructor(string memory name, string memory symbol) ERC721(name, symbol) {
-    //     tokenCounter = 0; // count of NFT equals 0 when we deploy contract
-    // }
-
-
-    function mintNFT(string memory tokenURI) public returns (uint256){
-        uint256 newItemId = tokenCounter;
-
-        // create new NFT by owner(sender) and new NFT id
-        _mint(msg.sender, newItemId);
-        _setTokenURI(newItemId, tokenURI);
-        
-        tokenCounter++; // increase count of NFT
-
-        return newItemId;
-    }
 }
 
 // abstract contract AuctionStructure is AuctionBox, Auction{
