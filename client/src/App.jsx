44c0c1a965b811bae6fa1cc71b3de242dacd5428
Color-Kat <katik7979@gmail.com>
2022-01-29 21:21:12 +0300

    web3 init

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,14 +2,13 @@ import { useContext, useState } from 'react';
 import { AuctionContext } from './context/AuctionContext';
 
 function App() {
-  const { test } = useContext(AuctionContext);
+  // const { test } = useContext(AuctionContext);
 
-  console.log(test);
+  // console.log(test);
 
   return (
     <div className="App">
       <h2>Hello, nigger</h2>
-      <h3>{test}</h3>
     </div>
   )
 }
--- client/src/context/AuctionContext.jsx
+++ client/src/context/AuctionContext.jsx
@@ -1,10 +1,39 @@
 import React from "react";
+import Web3 from 'web3';
 
 export const AuctionContext = React.createContext();
 
-export const AuctionProvider = ({children}) => {
+const initWeb3 = () => {
+    if (window.ethereum) {
+        window.web3 = new Web3(ethereum);
+
+        try {
+            // request user access to account
+            ethereum.enable();
+            return true;
+        } catch (error) {
+            console.log("We don't have permission!", error);
+            return false;
+        }
+    } else if (window.web3) {
+        window.web3 = new Web3(web3.currentProvider);
+        return true;
+    } else {
+        if (confirm('You need to instal metamask. Click "ok" to do that')) {
+            window.open('https://chrome.google.com/webstore/detail/metamask/nkbihfbeogaeaoehlefnkodbefgpgknn');
+        }
+
+        return false;
+    }
+}
+
+export const AuctionProvider = ({ children }) => {
+    initWeb3();
+
+    console.log(web3);
+
     return (
-        <AuctionContext.Provider value={{test: "123"}}>
+        <AuctionContext.Provider value={{}}>
             {children}
         </AuctionContext.Provider>
     )
