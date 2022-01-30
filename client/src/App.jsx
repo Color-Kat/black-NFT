95c92b054194cca046cb26724979099e096e222c
Color-Kat <katik7979@gmail.com>
2022-01-30 22:03:39 +0300

    ethers migration

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,7 @@ import { useContext, useEffect, useState } from 'react';
 import { AuctionContext } from './context/AuctionContext';
 
 function App() {
-  const { installMetamask, getAuctions } = useContext(AuctionContext);
+  const { error, installMetamask, getAuctions } = useContext(AuctionContext);
 
   // console.log(test);
 
@@ -11,6 +11,10 @@ function App() {
 
   return (
     <div className="App">
+      {error &&
+        <h1>It's ERROR, nigga!</h1>
+      }
+
       {installMetamask
         ? <h1>Install metamask, nigger!</h1>
         : <h2>Hello, nigger</h2>
--- client/src/context/AuctionContext.jsx
+++ client/src/context/AuctionContext.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import Web3 from "web3/dist/web3.min.js";
+import { ethers } from 'ethers';
 import {
     auctionBox_contract_ABI,
     auction_contract_ABI,
@@ -9,54 +9,26 @@ import {
 
 export const AuctionContext = React.createContext();
 
-/**
- * check if metamask(ethereum) or web3 already exist
- * if don't, create web3
- * or show "install metamask" message
- *
- * @returns boolean
- */
-// const initWeb3 = async () => {
-//     if (window.ethereum) {
-//         window.web3 = new Web3(ethereum);
-//         return true;
+const { ethereum } = window;
 
-//         // try {
-//         // request user access to account
-//         // ethereum.enable();
-//         // const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
-//         // return true;
-//         // } catch (error) {
-//         //     console.log("We don't have permission!", error);
-//         //     return false;
-//         // }
-//     } else if (window.web3) {
-//         window.web3 = new Web3(web3.currentProvider);
-//         return true;
-//     } else {
-//         if (confirm('You need to instal metamask. Click "ok" to do that')) {
-//             window.open(
-//                 "https://chrome.google.com/webstore/detail/metamask/nkbihfbeogaeaoehlefnkodbefgpgknn"
-//             );
-//         }
+const getEthereumContract = (abi, address) => {
+    const provider = new ethers.providers.Web3Provider(ethereum);
+    const signer = provider.getSigner();
+    const ethereumContract = new ethers.Contract(address, abi, signer);
 
-//         return false;
-//     }
-// };
+    return ethereumContract;
+}
 
-const auctionBox = () => {
-    if (!window.web3) return false;
-    return new web3.eth.Contract(auctionBox_contract_ABI, contractAddress);
+const auctionBoxContract = () => {
+    return getEthereumContract(auctionBox_contract_ABI, contractAddress);
 };
 
-const auction = (address) => {
-    if (!window.web3) return false;
-    return new web3.eth.Contract(auction_contract_ABI, address);
+const auctionContract = (address) => {
+    return getEthereumContract(auction_contract_ABI, address);
 };
 
-const nft = (address) => {
-    if (!window.web3) return false;
-    return new web3.eth.Contract(NFT_contract_ABI, address);
+const nftContract = (address) => {
+    return getEthereumContract(NFT_contract_ABI, address);
 };
 
 export const AuctionProvider = ({ children }) => {
@@ -64,40 +36,50 @@ export const AuctionProvider = ({ children }) => {
     const [installMetamask, setInstallMetamask] = useState(false);
     const [currentAccount, setCurrentAccount] = useState("");
     const [isLoading, setIsLoading] = useState(false);
+    const [error, setError] = useState(false);
 
     // auctions data
     const [auctions, setAuctions] = useState([]);
 
     const checkInstallMetamask = () => {
-        if (!window.ethereum) {
+        if (!ethereum) {
             setInstallMetamask(true);
             return false;
         }
         return true;
     }
 
-    const initWeb3 = () => {
-        if (checkInstallMetamask())
-            window.web3 = new Web3(ethereum);
-    }
+    // const initWeb3 = () => {
+    //     if (checkInstallMetamask())
+    //         window.web3 = new Web3(ethereum);
+    // }
 
+    /**
+     * try to connect to metamask and set currentAccount
+     */
     const connectAccount = async () => {
-        if (!checkInstallMetamask()) return;
+        try {
+            if (!checkInstallMetamask()) return;
 
-        let accounts = await web3.eth.getAccounts();
-        setCurrentAccount(accounts[0]);
+            const accounts = await ethereum.request({ method: 'eth_accounts' });
+
+            setCurrentAccount(accounts[0]);
+        } catch (error) {
+            console.log("Some error happened", error);
+            setError(true);
+        }
     }
 
     /**
      * get auctions list from blockchain
      */
     const getAuctions = async () => {
-        if (!checkInstallMetamask() || !window.web3) return;
+        if (!checkInstallMetamask()) return;
 
-        // // (getAuTCions - a typo)
-        let auct = await auctionBox().methods.getAutcions().call();
-        console.log(auct);
-        setAuctions(auct);
+        // (getAuTCions - a typo)
+        let auctionsList = await auctionBoxContract().getAutcions();
+
+        setAuctions(auctionsList);
     }
 
     const createAuction = async () => {
@@ -108,12 +90,13 @@ export const AuctionProvider = ({ children }) => {
     //     auctionBox.methods.getAutcions().call().then(res => console.log(res));
 
     useEffect(() => {
-        initWeb3();
         connectAccount();
     }, []);
 
     return (
         <AuctionContext.Provider value={{
+            error,
+            isLoading,
             installMetamask,
             getAuctions
         }}>{children}</AuctionContext.Provider>
