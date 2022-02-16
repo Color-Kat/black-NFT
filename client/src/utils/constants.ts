b581816fa5dd2b5ea1d40308d4fa62072a16a213
Color-Kat <katik7979@gmail.com>
2022-02-16 20:34:15 +0300

    deploy new contract and get ABI

--- client/src/utils/constants.ts
+++ client/src/utils/constants.ts
@@ -1,16 +1,13 @@
-import abiAuctionBox from './contracts/AuctionBox.json';
-import abiAuction from './contracts/Auction.json';
-
 import abiUsers from './contracts/Users.json';
 import abiUser from './contracts/User.json';
 import abiNFT from './contracts/NFT.json';
-
-export const auctionBox_contract_ABI: any = abiAuctionBox.abi;
-export const auction_contract_ABI: any = abiAuction.abi;
+import abiAuctions from './contracts/Auctions.json';
+import abiAuction from './contracts/Auction.json';
 
 export const users_contract_ABI: any = abiUsers.abi;
 export const user_contract_ABI: any = abiUser.abi;
 export const NFT_contract_ABI: any = abiNFT.abi;
+export const auctions_contract_ABI: any = abiAuctions.abi;
+export const auction_contract_ABI: any = abiAuction.abi;
 
-
-export const contractAddress: string = '0x25f1799Fe18fE859c69Ac275Bc0Ae0ffCE1Fe804';
+export const contractAddress: string = '0xaF94d606432C65BaBeA9f08C61C6E7ceE819Eb00';
