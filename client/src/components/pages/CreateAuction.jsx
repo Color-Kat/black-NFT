a47b971c23e47d87cbe2262b1ab4ae2d05ec258d
Color-Kat <katik7979@gmail.com>
2022-03-04 20:52:31 +0300

    nigga cards display

--- client/src/components/pages/CreateAuction.jsx
+++ client/src/components/pages/CreateAuction.jsx
@@ -3,6 +3,7 @@ import { useContext, useState } from "react";
 import Rodal from 'rodal';
 import 'rodal/lib/rodal.css';
 import { AuctionContext } from "../../context/AuctionContext";
+import { dataURI2string } from "../../utils/dataURI2string";
 
 function ALoader() {
     return (
@@ -26,30 +27,30 @@ export const CreateAuction = ({ }) => {
 
     useEffect(async () => {
         if (user) {
-            console.log(123);
             setIsLoading(true);
             const niggasIndexes = await user.getMyNiggasIndexes();
-            console.log(niggasIndexes);
+
+            // Wait for get nigga tokenURI for every niggaIndex
             const niggas = await Promise.all(niggasIndexes.map(async (niggaIndex) => {
-                console.log(niggaIndex);
                 return await user.getNiggaURIById(niggaIndex);
             }));
-            console.log(niggas);
 
             // const niggas = await Promise.all(niggasIndexes.map(async (niggaIndex) => {
             //     return await new Promise(async resolve => resolve(await user.getNiggaURIById(niggaIndex)));
             // }));
 
-            // console.log(niggas);
-            // setMyNiggasIndexes(niggas);
+            setMyNiggasIndexes(niggas);
             setIsLoading(false);
         }
     }, [user]);
 
 
     return (
-        <section id="create-auction-page" className="page">
+        <section id="create-auction-page" className="page ">
             <h1 className="create-auction__title text-4xl text-slate-400 font-bold mb-8">Создать аукцион</h1>
+
+            <span>Здесь вы можете продать негра другому человеку в формате аукциона.</span>
+
 
             {/* Modal */}
             <Rodal
@@ -67,16 +68,29 @@ export const CreateAuction = ({ }) => {
 
                 {step === 1 &&
                     <>
-                        <div className="create-auction__form bg-slate-700 px-4 rounded-2xl p-5 shadow-md flex-1 mt-5 md:mt-0 text-left">
+                        <div className="create-auction__form relative bg-slate-700 px-4 rounded-2xl p-5 shadow-md flex-1 mt-5 md:mt-0 text-left">
                             <h3 className="text-2xl font-bold text-slate-500 mb-3">Выберите негра:</h3>
-                            <div>
+                            <div className="w-full overflow-x-scroll no-scrollbar">
                                 {isLoading
                                     ? <ALoader />
-                                    : <div>
-                                        {myNiggasIndexes.map(async niggaURI => {
-                                            // const niggaURI = await user.getNiggaURIById(niggaIndex);
-                                            console.log(niggaURI);
-                                            return (123);
+                                    : <div className="flex">
+                                        {myNiggasIndexes.map(tokenURIBase64 => {
+                                            // Get tokenURI in json format
+                                            const niggaTokenURI = JSON.parse(dataURI2string(tokenURIBase64));
+                                            const niggaSvg = dataURI2string(
+                                                niggaTokenURI.image, 'data:image/svg+xml;base64,'.length
+                                            );  // Get svg code of image from json
+
+                                            console.log(niggaTokenURI);
+                                            return (
+                                                <div className="nigga-card rounded-xl bg-gradient-to-r from-slate-900 via-purple-900 to-slate-900">
+                                                    <div className="nigga-card__image bg-slate-400 no-scrollbar max-h-96 max-w-xl overflow-auto" dangerouslySetInnerHTML={{ __html: niggaSvg }}></div>
+                                                    <div className="bg-slate-800">
+                                                        <h4>{niggaTokenURI.name}</h4>
+                                                        <h5>{niggaTokenURI.description}</h5>
+                                                    </div>
+                                                </div>
+                                            );
                                         })}
                                     </div>
                                 }
